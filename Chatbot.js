a048d9613099d7344ea9c5acf725de2df947a571
Admin <batuhan.oguz06@outlook.com>
2024-11-01 15:30:29 +0300

    installed node.js module

--- Chatbot.js
+++ Chatbot.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(express.static('public'));
 
 app.post('/api/chat', async (req, res) => {
     const message = req.body.message;

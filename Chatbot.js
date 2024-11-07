bebe2183a1bc48f1a702e705b7bbea2c6816cb40
Batuhan Oğuz <92732028+BatuhanOguzz@users.noreply.github.com>
2024-11-07 12:24:49 +0300

    Port change

--- Chatbot.js
+++ Chatbot.js
@@ -12,7 +12,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('public'));
